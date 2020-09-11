c03dd32271ee3ea0c436154c70d907cb64abd35f
yinhaiying <yinhaiying@58.com>
2020-09-11 14:55:24 +0800

    Button组件的实现二

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button, { ButtonSize, ButtonType } from "./packages/Button/button";
+import Button, { ButtonSize, ButtonType } from './packages/Button/button';
 function App() {
   return (
     <div className="App">
@@ -7,9 +7,9 @@ function App() {
         <h1>react study</h1>
         <Button>Hello</Button>
         <Button disabled>Hello</Button>
-        <Button type = {ButtonType.Primary} size = {ButtonSize.Large}>Hello</Button>
-        <Button type = {ButtonType.Link}  href = "http://www.baidu.com">百度</Button>
-        <Button type = {ButtonType.Link}  href = "http://www.baidu.com" disabled>百度</Button>
+        <Button type={ButtonType.Primary} size={ButtonSize.Large}>Hello</Button>
+        <Button type={ButtonType.Link}  href="http://www.baidu.com">百度</Button>
+        <Button type={ButtonType.Link}  href="http://www.baidu.com" disabled>百度</Button>
       </header>
     </div>
   );
--- src/packages/Button/button.tsx
+++ src/packages/Button/button.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import classNames from 'classnames';
 export enum ButtonSize {
     Large = 'lg',
-    smaill = 'sm'
+    Small = 'sm'
 }
 
 export enum ButtonType {
@@ -21,31 +21,31 @@ interface BaseButtonProps {
     href?:string;
 }
 
-const Button:React.FC<BaseButtonProps> =(props) => {
-    const { type, disabled, size, children,href} = props;
-    // btn,btn-lg,btn-primary
-    const classes = classNames("btn", {
-      [`btn-${type}`]: type,
-      [`btn-${size}`]: size,
-      // a标签没有disabled属性，因此需要手动添加disabled类。但是
-      // button是有disabled属性的。因此不需要添加这个类
-      "disabled": (type === ButtonType.Link) && disabled
-    });
+const Button:React.FC<BaseButtonProps> = (props) => {
+  const { type, disabled, size, children, href} = props;
+  // btn,btn-lg,btn-primary
+  const classes = classNames('btn', {
+    [`btn-${type}`]: type,
+    [`btn-${size}`]: size,
+    // a标签没有disabled属性，因此需要手动添加disabled类。但是
+    // button是有disabled属性的。因此不需要添加这个类
+    'disabled': (type === ButtonType.Link) && disabled
+  });
 
-    if(type === ButtonType.Link && href){
-        return (
-            <a className = {classes} href={href}>{children}</a>
-        )
-    }else{
-        return (
-            <button  className = {classes} disabled = {disabled}>{children}</button>
-        )
-    }
+  if (type === ButtonType.Link && href) {
+    return (
+      <a className={classes} href={href}>{children}</a>
+    )
+  } else {
+    return (
+      <button  className={classes} disabled={disabled}>{children}</button>
+    )
+  }
 }
 
 Button.defaultProps = {
-    disabled:false,
-    type:ButtonType.Default
+  disabled: false,
+  type: ButtonType.Default
 }
 
 export default Button
