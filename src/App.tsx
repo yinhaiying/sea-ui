fbce3a54c8f7ef9454dec329e801ad7cf5663204
yinhaiying <yinhaiying@58.com>
2020-09-11 14:47:53 +0800

    Button组件的初步一

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
-
+import Button, { ButtonSize, ButtonType } from "./packages/Button/button";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>react study</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button>Hello</Button>
+        <Button disabled>Hello</Button>
+        <Button type = {ButtonType.Primary} size = {ButtonSize.Large}>Hello</Button>
+        <Button type = {ButtonType.Link}  href = "http://www.baidu.com">百度</Button>
+        <Button type = {ButtonType.Link}  href = "http://www.baidu.com" disabled>百度</Button>
       </header>
     </div>
   );
--- src/packages/Button/button.tsx
+++ src/packages/Button/button.tsx
@@ -1,1 +1,51 @@
 import React from 'react'
+import classNames from 'classnames';
+export enum ButtonSize {
+    Large = 'lg',
+    smaill = 'sm'
+}
+
+export enum ButtonType {
+    Primary = "primary",
+    Default = 'default',
+    Danger = 'danger',
+    Link = 'link'
+}
+
+interface BaseButtonProps {
+    className ?:string;
+    disabled ?:boolean;
+    size?:ButtonSize;
+    type?:ButtonType;
+    children:React.ReactNode;
+    href?:string;
+}
+
+const Button:React.FC<BaseButtonProps> =(props) => {
+    const { type, disabled, size, children,href} = props;
+    // btn,btn-lg,btn-primary
+    const classes = classNames("btn", {
+      [`btn-${type}`]: type,
+      [`btn-${size}`]: size,
+      // a标签没有disabled属性，因此需要手动添加disabled类。但是
+      // button是有disabled属性的。因此不需要添加这个类
+      "disabled": (type === ButtonType.Link) && disabled
+    });
+
+    if(type === ButtonType.Link && href){
+        return (
+            <a className = {classes} href={href}>{children}</a>
+        )
+    }else{
+        return (
+            <button  className = {classes} disabled = {disabled}>{children}</button>
+        )
+    }
+}
+
+Button.defaultProps = {
+    disabled:false,
+    type:ButtonType.Default
+}
+
+export default Button
